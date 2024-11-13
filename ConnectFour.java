66cb38a88ff2b9e3951635204f5b47f3edeb1c5d
Sebastian Rivera <dietmilkstudios@gmail.com>
2024-11-13 12:25:22 -0500

    Create ConnectFour.java

---
+++ ConnectFour.java
@@ -0,0 +1,18 @@
+public class ConnectFour {
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * Our display method!
+     * @param column
+     */
+    public static void display(int column) {
+            
+    }
+
+    /** */
+    public static void dropChecker() {
+
+    }
+}
