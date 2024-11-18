a1cb762d0d6567b15f75736388c86e0782a2d7d9
Sebastian Rivera <dietmilkstudios@gmail.com>
2024-11-18 11:05:43 -0500

    💡 Added some comments

--- ConnectFour.java
+++ ConnectFour.java
@@ -1,3 +1,9 @@
+/**
+ * Our Connect Four Game for the comprehensive project!
+ * @author Ashika Kamjula, Sebastian Rivera, Emre Yilmazer, Layton Laurita
+ * @since Wed, Nov 13 (12:30PM)
+ */
+
 public class ConnectFour {
     public static void main(String[] args) {
 
@@ -5,13 +11,15 @@ public class ConnectFour {
 
     /**
      * Our display method!
-     * @param column
+     * @param column controls the columns for the board
      */
     public static void display(int column) {
             
     }
 
-    /** */
+    /**
+     * DropChecker
+     */
     public static void dropChecker() {
 
     }
