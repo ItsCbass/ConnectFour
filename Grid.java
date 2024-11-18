aa0757f50c1f9811cdead1fc1b6b87b0859369cd
Sebastian Rivera <dietmilkstudios@gmail.com>
2024-11-18 11:57:54 -0500

    📝 Added comment on top

--- Grid.java
+++ Grid.java
@@ -1,3 +1,8 @@
+/**
+ * Our Connect Four Game for the comprehensive project!
+ * @author Ashika Kamjula, Sebastian Rivera, Emre Yilmazer, Layton Laurita
+ */
+
 public class Grid {
     // SHOWS THE BOARD
 }
