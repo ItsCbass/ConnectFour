25a39139e8c37b4e59d1aaeb2940d714e13d4e5b
Emre-yr <emreyilmazer2004@gmail.com>
2024-11-18 11:46:28 -0500

    Update ConnectFour.java

--- ConnectFour.java
+++ ConnectFour.java
@@ -5,10 +5,30 @@
  */
 
 public class ConnectFour {
+
+    /** Number of pieces needed to be connected to win */
+    public static final int PIECES_TO_WIN = 4;
+
+    /** Column size of the grid */
+    public static final int COLUMN_SIZE = 8;
+
+    /** Row size of the grid */
+    public static final int ROW_SIZE = 8;
+    
+    public static final char EMPTY_SLOT = ' ';
+    
     public static void main(String[] args) {
 
     }
 
+    /**
+     * Makes players choose valid markers
+     * @param scanner 
+     */
+    public static void setupPlayer(Scanner scanner) {
+
+    }
+    
     /**
      * Our display method!
      * @param column controls the columns for the board
@@ -21,6 +41,14 @@ public class ConnectFour {
      * DropChecker
      */
     public static void dropChecker() {
+
+    }
+
+    /**
+     * Checks if the move is valid or not
+     * @param column the column that the player places the checker on
+     */
+    public static boolean validateMove(int column) {
 
     }
 }
