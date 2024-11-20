28f3920b65d84d6446c669dece058a36a2c488f7
Sebastian Rivera <dietmilkstudios@gmail.com>
2024-11-20 11:01:12 -0500

    ✨ Implemented Player.java

--- Player.java
+++ Player.java
@@ -3,6 +3,47 @@
  * @author Ashika Kamjula, Sebastian Rivera, Emre Yilmazer, Layton Laurita
  */
 
-public class Player {
-    // MAKES THE TURNS
-}
+ public class Player {
+    /** Sets the player_one var to 1 */
+    public static final int PLAYER_ONE = 1;
+
+    /** Sets the player_two var to 2 */
+    public static final int PLAYER_TWO = 2;
+
+    /** Instance Fields */
+    private int id;
+    private String name;
+
+    /**
+     * Constructs player, and checks to make sure that the player ID/name is valid
+     * @param id takes a player id
+     * @param name takes a player name
+     */
+    public Player(int id, String name) {
+        // Invalid ID!!
+        if (id != PLAYER_ONE && id != PLAYER_TWO) {
+            throw new IllegalArgumentException("Invalid player ID");
+        }
+
+        // Empty name case
+        if (name == null || name.trim().isEmpty()) {
+            throw new IllegalArgumentException("Player name cannot be empty");
+        }
+        this.id = id;
+        this.name = name.trim();
+    }
+
+    /**
+     * Getter for getting the player id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * Getter for getting the player name
+     */
+    public String getName() {
+        return name;
+    }
+ }
